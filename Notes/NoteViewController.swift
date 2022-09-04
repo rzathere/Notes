5b912e1a5cc8c9f598317db0997131f55ea59496
RZA <max.romance56@gmail.com>
2022-09-04 10:05:53 +0530

    Initial commit

---
+++ Notes/NoteViewController.swift
@@ -0,0 +1,20 @@
+
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var noteLabel: UITextView!
+
+    public var noteTitle: String = ""
+    public var note: String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = noteTitle
+        noteLabel.text = note
+    }
+
+
+}
